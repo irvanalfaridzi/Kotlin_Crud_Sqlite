c72b82d3131f9c1b0cbacd98424b272dce05a273
Alfaridzi <irvanalfaridzi@gmail.com>
2021-02-15 03:18:17 +0700

    Menu change filename

--- app/src/main/java/com/example/menumakananminuman/MenuActivity.kt
+++ app/src/main/java/com/example/menumakananminuman/MenuActivity.kt
@@ -3,9 +3,9 @@ package com.example.menumakananminuman
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class NoteActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_note)
+        setContentView(R.layout.activity_menu)
     }
 }
